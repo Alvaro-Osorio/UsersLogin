80da8095dba73a4a3f0790afce9c5a5689dd4aae
Alvaro-Osorio <alvaroosorioiba@gmail.com>
2024-01-31 17:37:45 -0600

    segundo commit

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,30 +1,43 @@
 import { useState } from "react";
 
-export const UserForm = () => {
-  const initialUserForm = {
-    username: "",
-    password: "",
-    email: "",
-  };
+const initialUserForm = {
+  username: "pepe",
+  password: "12345",
+  email: "pepe@correo.com",
+};
 
-  const [userForm, setUserForm] = useState();
+export const UserForm = () => {
+  const [userForm, setUserForm] = useState(initialUserForm);
+
+  const { username, email, password } = userForm;
 
   const onInputChange = ({ target }) => {
-    console.log(target.value);
+    const { name, value } = target;
+
+    setUserForm({
+      ...userForm,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
   };
   return (
     <>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           className="form-control my-3 w-75"
           placeholder="Usuario"
           name="username"
+          value={username}
           onChange={onInputChange}
         />
         <input
           className="form-control my-3 w-75"
           placeholder="Email"
           name="email"
+          value={email}
           onChange={onInputChange}
         />
         <input
@@ -32,9 +45,12 @@ export const UserForm = () => {
           placeholder="Password"
           name="password"
           type="password"
+          value={password}
           onChange={onInputChange}
         />
-        <button className="btn btn-primary">Crear</button>
+        <button className="btn btn-primary" type="submit">
+          Crear
+        </button>
       </form>
     </>
   );
